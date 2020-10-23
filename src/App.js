72b9bcfcce2a7703e85bc7f7ce04a6d5c511141e
ablakerose <bdecktor@gmail.com>
2020-10-23 14:12:16 -0400

    add api key and search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
+const api = {
+  key: "52d4f34a06f5a4f41dbec660a0a2c8db", 
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <main>
+        <div className='search-box'>
+          <input
+          type='text'
+          className='search-bar'
+          placeholder= "Search..."
+          />
+        </div>
+      </main>
     </div>
   );
 }
